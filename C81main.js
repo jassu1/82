4001bedc42f3e9154a93c9fa506d3f234b521c2d
jassu1 <93320373+jassu1@users.noreply.github.com>
2022-09-29 17:40:30 +0930

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,33 @@
+canvas=document.getElementById("myCanvas");
+colour="red";
+ctx=canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle=colour;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
+    colour=document.getElementById("colour").value;
+    console.log(colour);
+
+    mouse_x= e.clientX - canvas.offsetLeft;
+    mouse_y= e.clientY - canvas.offsetTop;
+
+    circle(mouse_x, mouse_y);
+}
+
+function circle(mouse_x, mouse_y){
+    ctx.beginPath();
+ctx.strokeStyle=colour;
+ctx.lineWidth=2;
+ctx.arc(mouse_x, mouse_y, 40, 0, 2 * Math.PI);
+ctx.stroke();
+}
+
+function cleararea(mouse_x, mouse_y){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
